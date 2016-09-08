0603ad87697b06da79b2376cdcd3709a44767eef
warrior <wumenglong@aliyun.com>
2016-09-08 15:00:15 +0800

    图片上传
    2016.09.08 图片上传

--- UploadDemo1/ViewController.m
+++ UploadDemo1/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -19,9 +20,89 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showPhoto:(id)sender
+{
+//    UIImagePickerController 图片选择器
+    UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+    
+//    是否允许编辑
+    picker.allowsEditing = YES;
+    
+//    判断是否支持这个来源
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum]) {
+//        设置图片来源为系统相册
+        picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    }
+//    设置代理
+    picker.delegate = self;
+    
+//    弹出相册
+    [self presentViewController:picker animated:YES completion:nil];
 }
+
+- (IBAction)uploadPic:(id)sender
+{
+    NSString *urlStr = @"http://localhost:8080/DownloadAndUpload/upload";
+    
+    NSURL *url = [NSURL URLWithString:urlStr];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+//    把JPEG，JPG图片转化为二进制数据
+//    NSData *data = UIImageJPEGRepresentation(_imageView.image, 0.5);
+//    把PNG图片转化为二进制数据
+    NSData *data = UIImagePNGRepresentation(_imageView.image);
+    
+    [request setHTTPMethod:@"POST"];
+//    设置请求体
+    [request setHTTPBody:data];
+//    把数据的长度设置到请求头中
+    [request setValue:[NSString stringWithFormat:@"%ld",data.length] forHTTPHeaderField:@"Content-Length"];
+    
+    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:nil];
+    
+    [connection start];
+    
+}
+
+/*
+//iOS 3.0 被弃用
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(nullable NSDictionary<NSString *,id> *)editingInfo
+{
+    _imageView.image = image;
+//    选择图片之后不会自动返回，需要手动调用返回的方法
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+*/
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+//    获取原图
+//    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+//    获取编辑之后的图片
+    UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    _imageView.image = image;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
